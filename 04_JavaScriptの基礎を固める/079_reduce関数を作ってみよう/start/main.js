8329cc451cc5537312c09fdfa4ffe15f4829c62f
hiroshima820 <53898556+hiroshima820@users.noreply.github.com>
2021-04-24 13:24:53 +0900

    Finish section4

--- 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
@@ -1,9 +1,21 @@
-const str = 'animation';
-const strArry = str.split('');
+const str = "animation";
+const strArry = str.split("");
 
 function tag(accu, curr) {
-    return `${accu}<${curr}>`;
+  console.log(accu, curr);
+  return `${accu}<${curr}>`;
 }
 
-const result = strArry.reduce(tag, "");
+function reduce(arry, callback, defaultValue) {
+  let accu = defaultValue;
+
+  for (key in arry) {
+    let curr = arry[key];
+    accu = callback(accu, curr);
+  }
+
+  return accu;
+}
+
+const result = reduce(strArry, tag, "");
 console.log(result);
